2bb4d01343be7902d137f73e1d7a3dd45f038ed1
vnkl25 <vykle@lewisu.edu>
2025-10-29 13:30:32 -0500

    Set up Firebase Hosting with GitHub Actions

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
+  apiKey: "AIzaSyA58VZ9Z8UuUSBO-gvuiofvYijmKGdsfPk",
+  authDomain: "fir-auth-v3-a4a66.firebaseapp.com",
+  projectId: "fir-auth-v3-a4a66",
+  storageBucket: "fir-auth-v3-a4a66.firebasestorage.app",
+  messagingSenderId: "537801807751",
+  appId: "1:537801807751:web:ef8da88ef384c0ce5da522"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
