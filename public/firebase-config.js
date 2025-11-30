feed22ab9e724729b95833cb180c014be581c382
vnkl25 <vykle@lewisu.edu>
2025-11-30 11:45:35 -0600

    Add Firebase storage

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore, doc, setDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-storage.js";
 
 
 // Your Firebase configuration
@@ -18,9 +19,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 // Export so other scripts can use it
 export { auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged };
 export { db, doc, setDoc, collection, getDocs };
-
+export { storage, ref, uploadBytes, getDownloadURL, listAll } 
 
